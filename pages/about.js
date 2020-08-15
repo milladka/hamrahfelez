a6de8947b5fab6bc23412959e093e757b498fcc5
milladka <mihancivil@gmail.com>
2020-08-15 12:16:30 +0430

    add about and edit footer

--- pages/about.js
+++ pages/about.js
@@ -11,52 +11,33 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Milad Karikjnknmi</a>
+          درباره همراه فلز
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        شرکت همراه فلز آسیا با گذشت بیش از ۳۰ سال فعالیت در صنعت فولاد، حوزه ورق و لوله های فولادی و همچنین کسب رضایت از مشتریان، فعالیت خود را از سال 1397 بصورت رسمی آغاز نموده است.
+
+از بزرگترین اهداف همراه فلز ارائه مشاوره تخصصی برای مشتریان، ارائه محصول به سراسر کشور و همچنین مرجع قیمت گذاری در این حوزه می باشد. که تاکنون به این مهم نیز دست یافته است.
+
+همراه فلز با دارا بودن نماد های ساماندهی و اینماد مهر تاییدی بر فعالیت و رقابت پاک و سالم خود در این عرصه از سوی وزارت صنعت، معدن و تجارت دریافت نموده است.
+
+همچنین همراه فلز با دارا بودن کادری مجرب و حرفه ای در جهت ارائه خدمات به مشتریان نظیر مشاوره و فروش از افتخارات خود می داند که نامی ماندگار و خوش آوازه در صنعت ورق فولاد و لوله در خاطر و قلب مشتریان عزیز خود که ولینعمتان ما هستند به یادگار گذاشته.
+
+مهمترین رسالت همراه فلز ایجاد چرخه و زنجیره حیات صنعت فولاد و مشتقات آن در داخل کشور است و با بکار گیری پتانسیل و فضای موجود و همچنین رونق بخشیدن به تولید داخلی به دنبال تامین نیاز بازارهای داخلی و صادرات به دیگر کشورها در تمامی زمینه ها و صنایع گوناگون می باشد.
+
+همراه فلز با نیازسنجی بازار و همچنین تکیه بر تولید داخلی برخی از محصولات خود را بصورت برونسپاری تولید می نماید تا همواره خطوط تولید فولاد داخل کشور با اقتدار و صلابت خود به رونق و آبادانی این مرز و بوم کمک شایانی نمایند و نیاز به واردات فولاد و همچنین کالاهای وابسته به این صنعت را به صفر برسانند.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
+      <div className={styles.alert}>
+        وب سایت در حال بروزرسانی می باشد
+      </div>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      <a
+          href="https://vercel.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          ساخته شده با {' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
