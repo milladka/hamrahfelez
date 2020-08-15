339d7945b83a179923f5c61fbbd0ad85eae4348f
milladka <mihancivil@gmail.com>
2020-08-15 17:08:09 +0430

    omrani is best

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             <a>درباره ما</a>
           </Link>
           <Link href="/contact">
-            <a>تماس  با عمرانی</a>
+            <a>تماس  با ما</a>
           </Link>
         </div>       
        
